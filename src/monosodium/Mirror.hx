c47df6e9802db345448eac0b448c25f5b137c564
TheoFoxxo <80620538+Hackx2@users.noreply.github.com>
2026-01-05 18:00:43 +0000

    [chore]: use `url` method from mirror abstract

--- src/monosodium/Mirror.hx
+++ src/monosodium/Mirror.hx
@@ -2,6 +2,10 @@ package monosodium;
 
 // Despite everything, it's still you...
 enum abstract Mirror(String) from String to String {
-	final E621:Mirror = "e621";
-	final E926:Mirror = "e926";
+	final E621:Mirror = "e621"; // nsfw, sfw & questionable
+	final E926:Mirror = "e926"; // sfw & questionable
+
+	/// find an alternative 
+	public var url(get,never):String; // https://<mirror>.net
+	@:noCompletion function get_url():String return 'https://$this.net';
 }
