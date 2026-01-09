13d8e7fe93139424ac26109dec9a9f5254dd809a
TheoFoxxo <80620538+Hackx2@users.noreply.github.com>
2026-01-09 11:13:41 +0000

    [chore]: artist create query documentation

--- src/monosodium/endpoints/queries/artist/Create.hx
+++ src/monosodium/endpoints/queries/artist/Create.hx
@@ -1,29 +1,48 @@
 package monosodium.endpoints.queries.artist;
 
 typedef Create = {
+	/**
+	 * The artist's name.
+	 */
 	var name:String; // required
 
 	/**
-	 * Each entry is limited to 100 character.
-	 * Max 25 entries.
+	 * Artist aliases. \
+	 * Silently truncated to 25 entries and 100 characters.
 	 */
-	@:optional var other_names:Null<Array<String>>;  
+	@:optional var other_names:Null<Array<String>>; 
+	
+	/**
+	 * Separate names with spaces, not commas. Use underscores for spaces inside names. \
+	 * Silently truncated to 25 entries. 
+	 */
 	@:optional var other_names_string:Null<String>;
+
+
+	/**
+	 * The artist's URLs \
+	 * You can prefix a URL with `-` to mark it as dead. \
+	 * Silently truncated to 25 entries. 
+	 */
 	@:optional var url_string:Null<String>;
 
 	/**
-	 * 250,000 character limit
+	 * Artist description/notes. \
+	 * Silently truncated to the wiki page limit (**250,000**)
 	 */
 	@:optional var notes:Null<String>;
+
+	
 	@:optional var group_name:Null<String>;
 
 	/**
-	 * Janitor+
+	 * This parameter requires you to be **Janitor+**
 	 */
 	@:optional var linked_user_id:Null<Int>;
 
 	/**
-	 * Janitor+
+	 * Whether the artist is locked or not. \
+	 * This parameter requires you to be **Janitor+**
 	 */
 	@:optional var is_locked:Null<Bool>;
 }
