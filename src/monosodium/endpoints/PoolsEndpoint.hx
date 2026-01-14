8955a79b81351a99f704f8e434d4dfce1069f136
hackx2 <80620538+hackx2@users.noreply.github.com>
2026-01-14 02:07:56 +0000

    [feat]: complete pools endpoint

--- src/monosodium/endpoints/PoolsEndpoint.hx
+++ src/monosodium/endpoints/PoolsEndpoint.hx
@@ -1,17 +1,29 @@
 package monosodium.endpoints;
 
+import monosodium.endpoints.schemas.pools.Element;
 import monosodium.endpoints.schemas.pools.Create;
-import haxe.http.HttpMethod;
 import monosodium.endpoints.queries.Pools;
+import monosodium.endpoints.base.Endpoint;
 import monosodium.endpoints.schemas.Pool;
 import monosodium.Monosodium;
 import haxe.http.HttpMethod;
-import monosodium.endpoints.base.Endpoint;
 
+/**
+ * Endpoint for interacting with the `/pools` & '/pool_element' API route.
+ * This endpoint class provides methods to `search`, `create`, `get`, and `edit` artists.
+ * Additionally provides methods for `addPost`, `removePost`, and `recent`.
+ * 
+ * Janitor+/Admin+ endpoints are excluded from this class.
+ */
 @:route('/pools')
 final class PoolsEndpoint extends Endpoint {
-	public static inline final pool_element:String = '/pool_element';
-
+	/**
+	 * Search for pools based on query parameters.
+	 * 
+	 * @param params Search query parameters for filtering pools (e.g., id, order, name, etc)
+	 * @param callback Callback to handle an array of Artist object(s) returned from the API
+	 * @param onError (Optional) Callback to handle errors that occur during the request
+	 */
 	public function search(params:Pools, callback:Array<Pool>->Void, ?onError:String->Void):Void {
 		api.request('${route}.json', false, HttpMethod.Get, data -> {
 			try {
@@ -24,35 +36,72 @@ final class PoolsEndpoint extends Endpoint {
 		}, onError, null, params);
 	}
 
+	/**
+	 * Create a new pool.
+	 * 
+	 * @param params Pool creation parameters (e.g., name, notes, URLs, etc.)
+	 * @param callback Callback to handle the newly created Pool object returned from the API
+	 * @param onError (Optional) callback to handle errors that occur during the request
+	 */
+	public function create(data:Create, callback:Pool->Void, ?onError:String->Void):Void {
+		api.request('${route}.json', true, HttpMethod.Post, callback, onError, null, null, {pool: data});
+	}
+
+	/**
+	 * Retrieve a pool by its ID.
+	 * 
+	 * @param id The unique ID of the pool to retrieve
+	 * @param callback Callback to handle the pool object returned from the API
+	 * @param onError (Optional) Callback to handle errors that occur during the request
+	 */
 	public function get(id:Int, callback:Pool->Void, ?onError:String->Void):Void {
 		api.request('${route}/${id}.json', false, HttpMethod.Get, data -> callback((cast data : Pool)), onError);
 	}
 
-	public function create(data:Create, callback:Pool->Void, ?onError:String->Void):Void {
-		api.request('${route}.json', true, HttpMethod.Post, callback, onError, null, null, data);
+	/**
+	 * Edit an existing pool.
+	 * 
+	 * @param id The unique ID of the pool to edit
+	 * @param params Updated pool parameters (e.g., name, notes, URLs, etc.)
+	 * @param callback Callback to handle the updated Pool object returned from the API
+	 * @param onError (Optional) Callback to handle errors that occur during the request
+	 */
+	public function edit(id:Int, data:Create, callback:Pool->Void, ?onError:String->Void):Void {
+		api.request('${route}/${id}.json', true, HttpMethod.Patch, callback, onError, null, null, {pool: data});
 	}
 
-	public function update(id:Int, data:Create, callback:Pool->Void, ?onError:String->Void):Void {
-		api.request('${route}/${id}.json', true, HttpMethod.Patch, callback, onError, null, null, data);
+	@:dox(hide) static inline final elementRoute:String = '/pool_element';
+
+	/**
+	 * Add a post to the given pool.
+	 * 
+	 * @param params Add post parameters (e.g pool_id, pool_name, post_id, etc.)
+	 * @param callback Callback to handle the post object returned from the API
+	 * @param onError (Optional) Callback to handle errors that occur during the request
+	 */
+	public function addPost(params:Element, callback:Pool->Void, ?onError:String->Void):Void {
+		api.request('${elementRoute}.json', true, HttpMethod.Post, callback, onError, null, null, params);
 	}
 
-	public function delete(id:Int, callback:Void->Void, ?onError:String->Void):Void {
-		api.request('${route}/${id}.json', true, HttpMethod.Delete, d -> callback(), onError);
+	/**
+	 * Removes a post from a pool.
+	 * 
+	 * @param params parameters
+	 * @param callback Callback to handle the post object returned from the API
+	 * @param onError (Optional) Callback to handle errors that occur during the request
+	 */
+	public function removePost(params:Element, callback:Void->Void, ?onError:String->Void):Void {
+		api.request('${elementRoute}.json', true, HttpMethod.Delete, d -> callback(), onError, null, null, params);
 	}
 
-	public function revert(id:Int, version_id:Int, callback:Void->Void, ?onError:String->Void):Void {
-		api.request('${route}/${id}/revert.json', true, HttpMethod.Put, d -> callback(), onError, null, {id: id, version_id: version_id});
-	}
-
-	public function addPost(pool_id:Int, post_id:Int, callback:Pool->Void, ?onError:String->Void):Void {
-		api.request('${pool_element}.json', true, HttpMethod.Post, callback, onError, null, {pool_id: pool_id, post_id: post_id});
-	}
-
-	public function removePost(pool_id:Int, post_id:Int, callback:Void->Void, ?onError:String->Void):Void {
-		api.request('${pool_element}.json', true, HttpMethod.Delete, d -> callback(), onError, null, {pool_id: pool_id, post_id: post_id});
-	}
-
-	public function recent(callback:Array<Dynamic>->Void, ?onError:String->Void):Void {
-		api.request('${pool_element}/recent.json', false, HttpMethod.Get, callback, onError, null);
+	/**
+	 * Get recent pools.
+	 * 
+	 * @param params parameters
+	 * @param callback Callback to handle the post object returned from the API
+	 * @param onError (Optional) Callback to handle errors that occur during the request
+	 */
+	public function recent(params:Element, callback:Array<Dynamic>->Void, ?onError:String->Void):Void {
+		api.request('${elementRoute}/recent.json', false, HttpMethod.Get, callback, onError, null, null, params);
 	}
 }
--- src/monosodium/endpoints/base/Endpoint.hx
+++ src/monosodium/endpoints/base/Endpoint.hx
@@ -2,7 +2,7 @@ package monosodium.endpoints.base;
 
 @:route('/') // isn't needed but eh
 @:autoBuild(monosodium.macros.RouteBuilder.build())
-class Endpoint {
+abstract class Endpoint {
 	@:dox(hide) var api:Monosodium;
 	public function new(api:Monosodium):Void this.api = api;
 }
