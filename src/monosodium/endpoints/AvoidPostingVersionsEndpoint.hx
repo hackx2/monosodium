b4d0f301b8618fe3312b3a42de5303dbd939497d
HackDev <william.patonlol@gmail.com>
2026-02-06 22:50:39 +0000

    [feat]: complete `/avoid_posting_versions`
    - completed `/avoid_posting_versions`
    - converted `structInit` class `monosodium.endpoints.schemas.AvoidPostingVersion` to normal typedef.

--- src/monosodium/endpoints/AvoidPostingVersionsEndpoint.hx
+++ src/monosodium/endpoints/AvoidPostingVersionsEndpoint.hx
@@ -5,12 +5,23 @@ import monosodium.endpoints.queries.AvoidPostingVersions;
 import monosodium.endpoints.schemas.AvoidPostingVersion;
 import monosodium.endpoints.base.Endpoint;
 
+/**
+ * Endpoint for interacting with the `/avoid_posting_versions` API route.
+ * This endpoint class provides a method to `search`.
+ */
 @:route('/avoid_posting_versions')
 final class AvoidPostingVersionsEndpoint extends Endpoint {
+	/**
+	 * Search for avoid posting versions entries based on query parameters.
+	 * 
+	 * @param params Search query parameters for filtering (e.g., id, order, name, etc)
+	 * @param callback Callback to handle an array of `AvoidPostingVersion` object(s) returned from the API
+	 * @param onError (Optional) Callback to handle errors that occur during the request
+	 */
 	public function search(params:AvoidPostingVersions, callback:Array<AvoidPostingVersion>->Void, ?onError:String->Void):Void {
 		api.request('${route}.json', false, HttpMethod.Get, data -> {
 			try {
-				callback((cast data : Array<Dynamic>).map(d -> return AvoidPostingVersion.sterilize(d)));
+				callback((cast data : Array<AvoidPostingVersion>));
 			} catch (e:Dynamic) {
 				if (onError != null) {
 					onError('Failed ${e}');
