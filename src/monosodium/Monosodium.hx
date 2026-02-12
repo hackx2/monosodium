f24d21a33513a3bf79ab0dc635a14728b0ab6af3
HackDev <william.patonlol@gmail.com>
2026-02-12 23:59:34 +0000

    [feat]: new lazy loading macro for endpoints

--- src/monosodium/Monosodium.hx
+++ src/monosodium/Monosodium.hx
@@ -12,6 +12,7 @@ import monosodium.net.ratelimiter.Limiter;
 import monosodium.net.RequestClient;
 
 // You are filled with determination
+@:build(monosodium.macros.LazyEndpointMacro.build())
 class Monosodium {
 	public static var defaultLimiter:Limiter = new Limiter(2);
 	public static var defaultVerboseMode:Bool = false;
@@ -24,21 +25,21 @@ class Monosodium {
 	private var _mirror:Null<Mirror>;
 	private var requestAPI:RequestClient;
 
-	public var posts:PostsEndpoint;
-	public var pools:PoolsEndpoint;
-	public var tags:TagsEndpoint;
-	public var users:UsersEndpoint;
-	public var bans:BansEndpoint;
-	public var artists:ArtistsEndpoint;
-	public var artistVersions:ArtistVersionsEndpoint;
-	public var artistUrls:ArtistUrlsEndpoint;
-	public var avoidPostings:AvoidPostingsEndpoint;
-	public var avoidPostingVersions:AvoidPostingVersionsEndpoint;
+	// Endpoints
+	@:lazy public var posts:PostsEndpoint;
+	@:lazy public var pools:PoolsEndpoint;
+	@:lazy public var tags:TagsEndpoint;
+	@:lazy public var users:UsersEndpoint;
+	@:lazy public var bans:BansEndpoint;
+	@:lazy public var artists:ArtistsEndpoint;
+	@:lazy public var artistVersions:ArtistVersionsEndpoint;
+	@:lazy public var artistUrls:ArtistUrlsEndpoint;
+	@:lazy public var avoidPostings:AvoidPostingsEndpoint;
+	@:lazy public var avoidPostingVersions:AvoidPostingVersionsEndpoint;
 
 	public function authorize(username:String, api_token:String, ?censor:Bool = true):Monosodium {
 		this.api_token = api_token;
 		this.username = username;
-
 		
 		final name:String = censor ? Utility.censorString(username) : username;
 		final token:String = censor ? Utility.censorString(api_token) : api_token;
@@ -49,21 +50,6 @@ class Monosodium {
 
 	public function new():Void {
 		this.requestAPI = new RequestClient(this);
-		initializeEndpoints();
-	}
-
-	private function initializeEndpoints() {
-		this.posts = new PostsEndpoint(this);
-		this.pools = new PoolsEndpoint(this);
-		this.tags = new TagsEndpoint(this);
-		this.artists = new ArtistsEndpoint(this);
-		this.users = new UsersEndpoint(this);
-		this.bans = new BansEndpoint(this);
-
-		this.artistVersions = new ArtistVersionsEndpoint(this);
-		this.artistUrls = new ArtistUrlsEndpoint(this);
-		this.avoidPostings = new AvoidPostingsEndpoint(this);
-		this.avoidPostingVersions = new AvoidPostingVersionsEndpoint(this);
 	}
 
 	public function mirror(mirror:Mirror = E621):Monosodium {
