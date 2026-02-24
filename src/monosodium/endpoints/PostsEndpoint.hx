120d01f9381ba9356fc36187e2f526c46d4f7fe6
HackDev <william.patonlol@gmail.com>
2026-02-24 23:13:52 +0000

    [feat]: completed posts endpoint

--- src/monosodium/endpoints/PostsEndpoint.hx
+++ src/monosodium/endpoints/PostsEndpoint.hx
@@ -1,120 +1,264 @@
 package monosodium.endpoints;
 
-import monosodium.endpoints.types.ID;
-import haxe.extern.EitherType;
+import haxe.io.Error;
 import haxe.http.HttpMethod;
-import monosodium.endpoints.schemas.post.Post as PostSchema;
-import monosodium.endpoints.queries.Posts;
-import monosodium.endpoints.schemas.post.Upload;
-import monosodium.endpoints.base.Endpoint;
+import haxe.extern.EitherType;
+
 import monosodium.endpoints.types.ID;
+import monosodium.endpoints.base.Query;
+import monosodium.endpoints.queries.Posts;
+import monosodium.endpoints.base.Endpoint;
+import monosodium.endpoints.schemas.post.Upload;
+import monosodium.endpoints.schemas.post.Response;
+import monosodium.endpoints.schemas.post.UpdatePost;
+import monosodium.endpoints.schemas.post.PostSequence;
+import monosodium.endpoints.schemas.post.FavoriteUser;
+import monosodium.endpoints.schemas.post.Post as PostSchema;
 
 using StringTools;
 
-// we're upto here ^w^
+/**
+ * Endpoint for interacting with the `/posts` & '/uploads.json' API routes.
+ * This endpoint class provides methods to `search`, `upload`, `random`, `get`, and `edit` posts.
+ * Additionally provides methods for `markAsTranslated`, `copyNotes`, `revert`, `showSequence`, and `favorites`.
+ * 
+ * Janitor+/Admin+ endpoints are excluded from this class.
+ */
 @:route('/posts')
 final class PostsEndpoint implements Endpoint {
-	public function upload(post:Upload, callback:Dynamic->Void, ?onError:String->Void):Void {
-		api.request('/uploads.json', true, HttpMethod.Post, response -> {
-			callback(response);
-		}, onError, null, post);
-	}
-
-	public function search(query:Posts, callback:Array<PostSchema>->Void, ?onError:String->Void):Void {
-		api.request('${route}.json', false, HttpMethod.Get, data -> {
-			try {
-				callback((data.posts : Array<PostSchema>));
-			} catch (e) {
-				if (onError != null) {
-					onError('failed sterilizing : $e');
+	/**
+	 * Search for post based on query parameters.
+	 * 
+	 * @param params Search query parameters for filtering post (e.g., id, order, name, etc)
+	 * @param callback Callback to handle an array of `Post` object(s) returned from the API
+	 * @param onError (Optional) Callback to handle errors that occur during the request
+	 */
+	@:GET function search(search_options:Posts, callback:Array<PostSchema>->Void, ?onError:String->Void):Void {
+		url:'${route}.json',
+		callbacks:{
+			success:(data) -> {
+				try {
+					callback((data.posts : Array<PostSchema>));
+				} catch (error:Error) {
+					if (onError != null) {
+						onError('Search failed: ${error}');
+					}
 				}
-			}
-		}, onError, null, query);
+			},
+			error:onError
+		},
+		params:search_options
 	}
 
-	public function get(id:ID, callback:PostSchema->Void, ?onError:String->Void):Void {
-		api.request('${route}/${id}.json', false, HttpMethod.Get, data -> {
-			try
-				callback((data.post : PostSchema))
-			catch (e)
-				if (onError != null)
-					onError('failed sterilizing : $e');
-		}, onError);
-	}
-
-	public function random(?tags:EitherType<Array<String>, String>, callback:PostSchema->Void, ?onError:String->Void):Void {
-		api.request('${route}/random.json', false, HttpMethod.Get, data -> {
-			try {
-				callback((data.post : PostSchema));
-			} catch (e)
-				if (onError != null) {
-					onError('failed sterilizing : $e');
+	/**
+	 * Get a random post.
+	 * 
+	 * @param tags Search tags
+	 * @param callback Callback to handle an array of `Post` object(s) returned from the API
+	 * @param onError (Optional) Callback to handle errors that occur during the request
+	 */
+	@:GET function random(?tags:EitherType<Array<String>, String>, callback:PostSchema->Void, ?onError:String->Void):Void {
+		url:'${route}/random.json',
+		callbacks:{
+			success:(data) -> {
+				try {
+					callback((data.post : PostSchema));
+				} catch (error:Error) {
+					if (onError != null) {
+						onError('Failed to get a random post: ${error}');
+					}
 				}
-		}, onError, null, {
-			tags: Std.isOfType(tags, String) ? (tags : String) : (tags : Array<String>).join(" ")
-		});
+			},
+			error:onError
+		},
+		params:{
+			tags:Std.isOfType(tags, String) ? (tags : String) : (tags : Array<String>).join(" ")
+		}
 	}
 
-	public function edit(id:ID, postEdit:Dynamic, callback:PostSchema->Void, ?onError:String->Void):Void {
-		api.request('${route}/${id}.json', true, HttpMethod.Patch, data -> {
-			try {
-				callback((data.post : PostSchema));
-			} catch (e) {
-				if (onError != null) {
-					onError('failed sterilizing : $e');
+	/**
+	 * Retrieve a post by its ID.
+	 * 
+	 * @param id The unique ID of the post to retrieve
+	 * @param callback Callback to handle the `Post` object returned from the API
+	 * @param onError (Optional) Callback to handle errors that occur during the request
+	 */
+	@:GET function get(id:ID, callback:PostSchema->Void, ?onError:String->Void):Void {
+		url:'${route}/${id}.json',
+		callbacks:{
+			success:(data) -> {
+				try {
+					callback((data.post : PostSchema));
+				} catch (error:Error) {
+					if (onError != null) {
+						onError('Failed to get post:${id}: ${error}');
+					}
 				}
-			}
-		}, onError, null, {post: postEdit});
+			},
+			error:onError
+		}
 	}
 
-	public function editIqdb(id:ID, callback:PostSchema->Void, ?onError:String->Void):Void {
-		api.request('${route}/$id/update_iqdb.json', true, HttpMethod.Get, data -> {
-			try {
-				callback((data.post : PostSchema));
-			} catch (e:Dynamic) {
-				if (onError != null) {
-					onError('failed sterilizing : $e');
+	/**
+	 * Edit an existing post.
+	 * 
+	 * @param id The unique ID of the Post to edit
+	 * @param params Updated Post upload parameters (e.g., tag_string, source, description, etc.)
+	 * @param callback Callback to handle the updated `Post` object returned from the API
+	 * @param onError (Optional) Callback to handle errors that occur during the request
+	 */
+	@:PATCH function edit(id:ID, postEdit:UpdatePost, callback:PostSchema->Void, ?onError:String->Void):Void {
+		url:'${route}/${id}.json',
+		callbacks:{
+			success:data -> {
+				try {
+					callback((data.post : PostSchema));
+				} catch (error:Error) {
+					if (onError != null) {
+						onError('An error occurred while trying to edit post: ${error}');
+					}
 				}
-			}
-		}, onError);
+			},
+			error:onError
+		},
+		body:{
+			post:postEdit
+		}
 	}
 
-	public function markAsTranslated(id:ID, translationCheck:Bool, partiallyTranslated:Bool, callback:PostSchema->Void, ?onError:String->Void):Void {
-		api.request('${route}/$id/mark_as_translated.json', true, HttpMethod.Post, data -> {
-			try {
-				callback((data.post : PostSchema));
-			} catch (e) {
-				if (onError != null) {
-					onError('failed sterilizing : $e');
+	/**
+	 * Mark a post as translated.
+	 * 
+	 * @param id The unique ID of the Post
+	 * @param translationCheck 
+	 * @param partiallyTranslated 
+	 * @param callback Callback to handle the updated `Post` object returned from the API
+	 * @param onError (Optional) Callback to handle errors that occur during the request
+	 */
+	@:POST function markAsTranslated(id:ID, translationCheck:Bool, partiallyTranslated:Bool, callback:PostSchema->Void, ?onError:String->Void):Void {
+		url:'${route}/$id/mark_as_translated.json',
+		callbacks:{
+			success:data -> {
+				try {
+					callback((data.post : PostSchema));
+				} catch (e) {
+					if (onError != null) {
+						onError('failed sterilizing : $e');
+					}
 				}
-			}
-		}, onError, null, {
-			translation_check: translationCheck,
-			partially_translated: partiallyTranslated
-		});
+			},
+			error:onError
+		},
+		body:{
+			translation_check:translationCheck,
+			partially_translated:partiallyTranslated
+		}
 	}
 
-	public function copyNotes(id:ID, otherPostId:Int, callback:Void->Void, ?onError:String->Void):Void {
-		api.request('${route}/$id/copy_notes.json', true, HttpMethod.Put, _ -> callback(), onError, null, {other_post_id: otherPostId});
+	/**
+	 * Copy notes to another post.
+	 * 
+	 * @param id The unique ID of the Post
+	 * @param otherPostId The unique ID of the other Post
+	 * @param callback
+	 * @param onError (Optional) Callback to handle errors that occur during the request
+	 */
+	@:PUT function copyNotes(id:ID, otherPostId:Int, callback:Void->Void, ?onError:String->Void):Void {
+		url:'${route}/$id/copy_notes.json',
+		callbacks:{
+			success:_ -> callback(),
+			error:onError
+		},
+		body:{
+			other_post_id:otherPostId
+		}
 	}
 
-	public function revert(id:ID, versionId:Int, callback:Void->Void, ?onError:String->Void):Void {
-		api.request('${route}/${id}/revert.json?version_id=${versionId}', true, HttpMethod.Post, _ -> callback(), onError);
+	/**
+	 * Revert a post.
+	 * 
+	 * @param id The unique ID of the Post.
+	 * @param versionId 
+	 * @param callback 
+	 * @param onError (Optional) Callback to handle errors that occur during the request.
+	 */
+	@:POST function revert(id:ID, versionId:Int, callback:Void->Void, ?onError:String->Void):Void {
+		url:'${route}/${id}/revert.json?version_id=${versionId}',
+		callbacks:{
+			success:_ -> callback(),
+			error:onError
+		},
+		params:{
+			version_id:versionId
+		}
 	}
 
-	public function showSeq(id:ID, dir:String, callback:PostSchema->Void, ?onError:String->Void):Void {
-		api.request('${route}/${id}/show_seq.json?seq=$dir', false, HttpMethod.Get, data -> {
-			try {
-				callback((data.post : PostSchema));
-			} catch (error:Dynamic) {
-				if (onError != null) {
-					onError('failed sterilizing post seq : $error');
+	/**
+	 * Get a post in sequence.
+	 * 
+	 * @param id The unique ID of the Post.
+	 * @param seq Either `NEXT` or `PREVIOUS`.
+	 * @param callback 
+	 * @param onError (Optional) Callback to handle errors that occur during the request.
+	 */
+	@:GET function showSequence(id:ID, seq:PostSequence, callback:PostSchema->Void, ?onError:String->Void):Void {
+		url:'${route}/${id}/show_seq.json',
+		callbacks:{
+			success:(data) -> {
+				try {
+					callback((data.post : PostSchema));
+				} catch (error:Error) {
+					if (onError != null) {
+						onError('Failed getting sequenced post: $error');
+					}
 				}
-			}
-		}, onError);
+			},
+			error:onError
+		},
+		params:{
+			seq:seq
+		}
 	}
 
-	public function unflag(id:ID, callback:Void->Void, ?onError:String->Void):Void {
-		api.request('${route}/${id}/flags.json', true, HttpMethod.Delete, _ -> callback(), onError, null, {approval: "approve"});
+	/**
+	 * Get a list of users who favorited a post.
+	 * 
+	 * @param options Filter options
+	 * @param callback Callback to handle the newly created `Pool` object returned from the API
+	 * @param onError (Optional) callback to handle errors that occur during the request
+	 */
+	@:GET function favorites(id:ID, filterOptions:Query, callback:Array<FavoriteUser>->Void, ?onError:String->Void):Void {
+		url:'/posts/${id}/favorites.json',
+		callbacks:{
+			success:(data) -> {
+				try {
+					callback((data : Array<FavoriteUser>));
+				} catch (error:Dynamic) {
+					if (onError != null) {
+						onError('Failed to get users who favorited this post: ${error}');
+					}
+				}
+			},
+			error:onError,
+		},
+		params:filterOptions
+	}
+
+	/**
+	 * Upload a post.
+	 * 
+	 * @param params Post upload parameters (e.g., name, notes, URLs, etc.)
+	 * @param callback Callback to handle the newly created `Pool` object returned from the API
+	 * @param onError (Optional) callback to handle errors that occur during the request
+	 */
+	@:POST function upload(upload_options:Upload, callback:Response->Void, ?onError:String->Void):Void {
+		url:'/uploads.json',
+		callbacks:{
+			success:(response) -> {
+				callback((response : Response));
+			},
+			error:onError
+		},
+		body:upload_options
 	}
 }
