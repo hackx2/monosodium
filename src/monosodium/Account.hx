2e44a4eb44fa55e37d523897ed1d1d24aef9eeab
HackDev <william.patonlol@gmail.com>
2026-02-26 02:06:57 +0000

    [feat]: add `setUsername` & `setToken` methods to `monosodium.Account`

--- src/monosodium/Account.hx
+++ src/monosodium/Account.hx
@@ -1,9 +1,16 @@
 package monosodium;
 
-@:keep
 final class Account {
-	public var username:String;
-	@:unreflective public var token:String;
+	public var username(default, null):String;
+	@:unreflective public var token(default, null):String;
+
+	inline public function setUsername(username:String):String {
+		return this.username = username;
+	}
+
+	inline public function setToken(token:String):String {
+		return this.token = token;
+	}
 
 	public function new(username:String, token:String):Void {
 		this.username = username;
